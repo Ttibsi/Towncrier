b71c9dffa5aaf9087231c2f72e195dfb41a88907
Ttibsi <ashisbitt@icloud.com>
2025-03-26 13:06:37 +0000

    restructure building to include sqlite

--- nob.c
+++ nob.c
@@ -1,5 +1,5 @@
 #define NOB_IMPLEMENTATION
-#include "include/nob.h"
+#include "include/nob/nob.h"
 
 int main(int argc, char **argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
@@ -7,16 +7,24 @@ int main(int argc, char **argv) {
     const char* compiler = "gcc";
     const char* tools[] = { "towncrier", "peasant" };
 
+    int towncrier_src_count = 2;
+    const char* towncrier_src_files[] = {
+        "src/towncrier/towncrier.c",
+        "include/sqlite/sqlite3.c"
+    };
+
     if (!nob_mkdir_if_not_exists("build/")) return 1;
 
     Nob_Cmd cmd = {0};
-    for (int i = 0; i < NOB_ARRAY_LEN(tools); i++) {
-        const char* build_file = nob_temp_sprintf("build/%s", tools[i]);
-        const char* source_file = nob_temp_sprintf("src/%s/%s.c", tools[i], tools[i]);
-        nob_cmd_append(&cmd, compiler, "-Wall", "-Wextra", "-o", build_file, source_file);
 
-        if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
+    nob_cmd_append(&cmd, compiler, "-Wall", "-Wextra", "-I", "include", "-o", "build/towncrier");
+    
+    for (int i = 0; i < towncrier_src_count; i++) {
+        nob_cmd_append(&cmd, towncrier_src_files[i]);
     }
+    if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
 
+    nob_cmd_append(&cmd, compiler, "-Wall", "-Wextra", "-I", "include", "-o", "build/peasant", "src/peasant/peasant.c");
+    if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
     return 0;
 }
--- src/towncrier/towncrier.c
+++ src/towncrier/towncrier.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+#include "sqlite/sqlite3.h"
+
+// [ ] Include sqlite, if a db doesn't exist, create it
+// [ ] When time is right (sunday, midnight), fork and run all-repos
+// [ ] Once all-repos runs, update db with last all-repos run time
+// [ ] DB also tracks when each backup was made
+// [ ] When it recieves a "hello" ping fro Peasant, return a message with all the current status values
+
 int main() {
     printf("Hello from the server\n");
 }
