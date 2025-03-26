61cb0c1a945b715a0c7c19c11ffa44c9bade4301
Ttibsi <ashisbitt@icloud.com>
2025-03-26 13:06:37 +0000

    create db and prep for setup

--- src/towncrier/towncrier.c
+++ src/towncrier/towncrier.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #include "sqlite/sqlite3.h"
+
+#define DB_NAME "towncrier.db"
 
 // [ ] Include sqlite, if a db doesn't exist, create it
 // [ ] When time is right (sunday, midnight), fork and run all-repos
@@ -8,6 +11,19 @@
 // [ ] DB also tracks when each backup was made
 // [ ] When it recieves a "hello" ping fro Peasant, return a message with all the current status values
 
+void setup_database(sqlite3* db) {
+}
+
 int main() {
-    printf("Hello from the server\n");
+    sqlite3* db;
+    if (sqlite3_open(DB_NAME, &db)) {
+        printf("Could not open " DB_NAME "\n");
+        return 1;
+    }
+
+    if (access(DB_NAME, F_OK) != 0) {
+        setup_database(&db);
+    }
+
+    return 0;
 }
