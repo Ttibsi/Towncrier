d359e0d5b93a5ca06c98e1e172375c13ad0e7ce5
ttibsi <ashisbitt@icloud.com>
2025-04-13 18:37:56 +0100

    setup argument parsing

--- src/peasant/peasant.c
+++ src/peasant/peasant.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    printf("Hello from the client\n");
+#include "sqlite/sqlite3.h>
+
+int cmp_arg(const char* arg, const char* inp) {
+    return strncmp(arg, inp) == 0;
+}
+
+void usage(void) {
+    printf("Something goes here\n");
+}
+
+int main(int argc, char* argv) {
+    if (argc == 1) {
+        // TODO: something
+        return 0;
+    }
+
+    if (cmp_arg(argv[1], "backup") {
+    } else if (cmp_arg(argv[1], "--help") {
+        usage();
+        return 0;
+    } else {
+        usage();
+        return 0;
+    }
 }
