8330bf126675c936919ef014647c8168e552ad31
ttibsi <ashisbitt@icloud.com>
2025-04-15 21:16:53 +0100

    restructure build system

--- nob.c
+++ nob.c
@@ -1,30 +1,43 @@
 #define NOB_IMPLEMENTATION
 #include "include/nob/nob.h"
 
-int main(int argc, char **argv) {
-    NOB_GO_REBUILD_URSELF(argc, argv);
-
+void cflags(Nob_Cmd* cmd) {
     const char* compiler = "gcc";
-    const char* tools[] = { "towncrier", "peasant" };
+    nob_cmd_append(cmd, compiler, "-Wall", "-Wextra", "-I", "include", "-o");
+}
 
-    int towncrier_src_count = 2;
-    const char* towncrier_src_files[] = {
+int build_towncrier(Nob_Cmd* cmd) {
+    cflags(cmd);
+    nob_cmd_append(
+        cmd,
+        "build/towncrier",
         "src/towncrier/towncrier.c",
         "include/sqlite/sqlite3.c"
-    };
+    );
 
+    if (!nob_cmd_run_sync_and_reset(cmd)) return 1;
+    return 0;
+}
+
+int build_peasant(Nob_Cmd* cmd) {
+    cflags(cmd);
+    nob_cmd_append(
+        cmd,
+        "build/peasant",
+        "src/peasant/peasant.c"
+    );
+
+    if (!nob_cmd_run_sync_and_reset(cmd)) return 1;
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    NOB_GO_REBUILD_URSELF(argc, argv);
     if (!nob_mkdir_if_not_exists("build/")) return 1;
 
     Nob_Cmd cmd = {0};
 
-    nob_cmd_append(&cmd, compiler, "-Wall", "-Wextra", "-I", "include", "-o", "build/towncrier");
-    
-    for (int i = 0; i < towncrier_src_count; i++) {
-        nob_cmd_append(&cmd, towncrier_src_files[i]);
-    }
-    if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
-
-    nob_cmd_append(&cmd, compiler, "-Wall", "-Wextra", "-I", "include", "-o", "build/peasant", "src/peasant/peasant.c");
-    if (!nob_cmd_run_sync_and_reset(&cmd)) return 1;
+    if (build_peasant(&cmd)) return 1;
+    if (build_towncrier(&cmd)) return 1;
     return 0;
 }
